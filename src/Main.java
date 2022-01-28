498d21b081093c439f8aa6a8077b328c35682009
Nikita Stroganov <54814796+IdeaSeeker@users.noreply.github.com>
2022-01-28 11:12:47 +0300

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static int example(int[] a, int i) {
+        if (i == -2)
+            throw new RuntimeException();
+        int tmp = 1 / i;
+        return a[i] + tmp;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}

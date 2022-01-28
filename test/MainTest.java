a2e77e1387e87d717714864eecec83c7093a4ea0
Nikita Stroganov <54814796+IdeaSeeker@users.noreply.github.com>
2022-01-28 12:16:07 +0300

    Update MainTest.java

--- test/MainTest.java
+++ test/MainTest.java
@@ -1,8 +1,6 @@
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 public class MainTest {
     @Test
     @DisplayName("main: -> PrintStreamPrintln")

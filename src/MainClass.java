ad2f980ff55e0cd406b94dad3d0b4d556ac39659
Nikita Stroganov <54814796+IdeaSeeker@users.noreply.github.com>
2022-01-31 11:17:49 +0300

    Update MainClass.java

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,5 +1,17 @@
 public class MainClass {
 
+    public static int callCallExample(boolean condition, int[] a, int i) {
+        return callExample(condition, a, i);
+    }
+
+    public static int callExample(boolean condition, int[] a, int i) {
+        if (condition) {
+            return example(a, i);
+        } else {
+            return example(a, i + 1);
+        }
+    }
+
     public static int example(int[] a, int i) {
         if (i == -2)
             throw new RuntimeException();
@@ -7,7 +19,21 @@ public class MainClass {
         return a[i] + tmp;
     }
 
+    static class Foo {
+        public static int n = 2;
+
+        int[] a;
+
+        Foo(int[] _a) {
+            a = _a;
+        }
+    }
+
+    public static int function(Foo foo) {
+        return 1 / foo.a[Foo.n];
+    }
+
     public static void main(String[] args) {
-        System.out.println(2);
+        System.out.println(1);
     }
 }

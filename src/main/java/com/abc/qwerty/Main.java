ed898a3b7d6e6a59b41af96d7273d724bc6c684e
Nikita Stroganov <54814796+IdeaSeeker@users.noreply.github.com>
2022-03-28 15:37:05 +0300

    Update Main.java

--- src/main/java/com/abc/qwerty/Main.java
+++ src/main/java/com/abc/qwerty/Main.java
@@ -1,33 +1,10 @@
 package com.abc.qwerty;
 
-import com.cba.ytrewq.Util;
-
 public class Main {
-
-    public enum Number {
-        ZERO,
-        NONZERO
-    }
-
-    public static int example(Util util, Number condition) {
-        switch (condition) {
-            case ZERO:
-                return util.multiply(0);
-            case NONZERO:
-                return util.multiply(100000);
-            default:
-                return 0;
-        }
-    }
-
-    public static int getByIndex(int[] array, int index) {
+    public static int myFunction(int[] array, int index) {
         if (index == -2)
             throw new RuntimeException();
         int tmp = 1 / index;
         return array[index] + tmp;
-    }
-
-    public static void main(String[] args) {
-
     }
 }

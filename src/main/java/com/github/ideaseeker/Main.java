04706f029d1e8acb5930ea3d7e529398f8bda1ae
Nikita Stroganov <54814796+IdeaSeeker@users.noreply.github.com>
2022-06-28 18:19:08 +0300

    Update Main.java

--- src/main/java/com/github/ideaseeker/Main.java
+++ src/main/java/com/github/ideaseeker/Main.java
@@ -7,20 +7,20 @@ import com.github.ideaseeker.util.Util;
 public class Main {
 
     public static void main(String[] args) {
-        MyLinkedList myList = new MyLinkedList(new Node(5));
-        myList.add(new Node(2));
-        myList.add(new Node(7));
-        System.out.println(myList);
-        // [5] -> [2] -> [7] ->
+//         MyLinkedList myList = new MyLinkedList(new Node(5));
+//         myList.add(new Node(2));
+//         myList.add(new Node(7));
+//         System.out.println(myList);
+//         // [5] -> [2] -> [7] ->
 
-        myList.squareElements();
-        System.out.println(myList);
-        // [25] -> [4] -> [49] ->
+//         myList.squareElements();
+//         System.out.println(myList);
+//         // [25] -> [4] -> [49] ->
 
-        int[] sortedList = Util.bubbleSort(myList.toArray());
-        for (int value : sortedList) {
-            System.out.print(value + " ");
-        }
-        // 4 25 49
+//         int[] sortedList = Util.bubbleSort(myList.toArray());
+//         for (int value : sortedList) {
+//             System.out.print(value + " ");
+//         }
+//         // 4 25 49
     }
 }
